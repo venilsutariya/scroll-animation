26e01d44360901a9252073e9652a37578951cf8c
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2025-06-30 17:50:19 +0530

    complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,6 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className} antialiased`}
       >
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,182 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useRef } from "react";
+import ReactLenis from "@studio-freight/react-lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const containerRef = useRef(null);
+  const stickyRef = useRef(null);
+  const handContainerRef = useRef(null);
+  const handRef = useRef(null);
+  const handImageRef = useRef(null);
+  const introRef = useRef(null);
+  const h1ElementRef = useRef(null);
+  const introCopyRef = useRef(null);
+  const websiteContentRef = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const introHeaders = [
+    "<span>time to</span> be brave",
+    "<span>time to</span> be playful",
+    "<span>time to</span> design the future",
+    "<span>time to</span> meet harrnish",
+    "<span>time to</span> see project one",
+  ];
+
+  useGSAP(
+    () => {
+      let currentCycle = -1;
+      let imageRevealed = false;
+
+      const updateHeaderText = () => {
+        if (h1ElementRef.current) {
+          h1ElementRef.current.innerHTML =
+            introHeaders[Math.min(currentCycle, introHeaders.length - 1)];
+        }
+      };
+
+      const pinnedHeight = window.innerHeight * 8;
+
+      ScrollTrigger.create({
+        trigger: stickyRef.current,
+        start: "top top",
+        end: `+=${pinnedHeight}`,
+        pin: true,
+        pinSpacing: true,
+        onUpdate: (self) => {
+          const progress = self.progress;
+
+          const rotationProgress = Math.min((progress * 8) / 5, 1);
+          const totalRotation = rotationProgress * 1800 - 90;
+          const rotationInCycle = ((totalRotation + 90) % 360) - 90;
+          gsap.set(handContainerRef.current, { rotationZ: rotationInCycle });
+
+          const newCycle = Math.floor((totalRotation + 90) / 360);
+          if (
+            newCycle !== currentCycle &&
+            newCycle >= 0 &&
+            newCycle < introHeaders.length
+          ) {
+            currentCycle = newCycle;
+            updateHeaderText();
+
+            if (newCycle == 3 && !imageRevealed) {
+              gsap.to(handImageRef.current, { opacity: 1, duration: 0.3 });
+              gsap.to(introCopyRef.current.querySelectorAll("p"), {
+                x: 0,
+                opacity: 1,
+                duration: 0.5,
+                stagger: 0.1,
+              });
+              imageRevealed = true;
+            } else if (newCycle !== 3 && imageRevealed) {
+              gsap.to(handImageRef.current, { opacity: 0, duration: 0.3 });
+              gsap.to(introCopyRef.current.querySelectorAll("p"), {
+                x: 20,
+                opacity: 0,
+                duration: 0.5,
+                stagger: 0.1,
+              });
+              imageRevealed = false;
+            }
+          }
+
+          if (progress <= 6 / 8) {
+            const animationProgress = Math.max(0, (progress - 5 / 8) / (1 / 8));
+            const newHeight = gsap.utils.interpolate(
+              52.75,
+              100,
+              animationProgress
+            );
+
+            const newOpacity = gsap.utils.interpolate(1, 0, animationProgress);
+            gsap.set(handRef.current, { height: `${newHeight}%` });
+            gsap.set(introRef.current, { opacity: 1 });
+            gsap.set(h1ElementRef.current, { opacity: newOpacity });
+            gsap.set(h1ElementRef.current.querySelector("span"), {
+              opacity: newOpacity,
+            });
+          } else {
+            gsap.set(introRef.current, { opacity: 0 });
+          }
+
+          if (progress <= 7/8) {
+            const scaleProgress = Math.max(0, (progress - 6/8) / (1/8));
+            const newScale = gsap.utils.interpolate(1, 20, scaleProgress);
+            gsap.set(handRef.current, { scale: newScale });
+          }
+
+          if (progress <= 7.5/8) {
+            const opacityProgress = Math.max(0, (progress - 7/8) / (0.5 / 8));
+            const newOpacity = gsap.utils.interpolate(1, 0, opacityProgress);
+            gsap.set(handRef.current, { opacity: newOpacity });
+          }
+
+          if (progress > 7.5/8) {
+            const revealProgress = (progress - 7.5/8) / (0.5/8);
+            const newOpacity = gsap.utils.interpolate(0, 1, revealProgress);
+            gsap.set(websiteContentRef.current, { opacity: newOpacity });
+          } else {
+            gsap.set(websiteContentRef.current, { opacity: 0 });
+          }
+        },
+      });
+
+      updateHeaderText();
+
+      return () => {
+        ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+      }
+    },
+    { scope: containerRef }
+  );
+
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+      <div
+        className="container max-w-[100vw] overflow-hidden"
+        ref={containerRef}
+      >
+        <section className="sticky h-screen w-screen" ref={stickyRef}>
+          <div className="hand-container" ref={handContainerRef}>
+            <div className="hand" ref={handRef}>
+              <img
+                src="https://i0.wp.com/imaginewithrashid.com/wp-content/uploads/2024/03/rashidckk_Powerful_woman_portrait_strong_contrasts_bold_color_4fa2275b-1be5-4595-aa6f-9955d7bc90f7_3.webp?resize=1024%2C1024&ssl=1"
+                alt=""
+                ref={handImageRef}
+              />
+            </div>
+          </div>
+          <div className="intro" ref={introRef}>
+            <h1 ref={h1ElementRef}>
+              <span>time to</span> be brave
+            </h1>
+            <div ref={introCopyRef}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corrupti, odio? Accusamus cum quas voluptatum nihil adipisci
+                doloremque magnam, iusto architecto.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+                odit deleniti veniam quas maiores earum iusto modi perferendis
+                excepturi velit.
+              </p>
+            </div>
+          </div>
+
+          <div className="website-content" ref={websiteContentRef}>
+            <h1>Venil</h1>
+          </div>
+        </section>
+        <section className="about h-screen w-screen">
+          <p>next section goes here</p>
+        </section>
+      </div>
+    </ReactLenis>
   );
 }
